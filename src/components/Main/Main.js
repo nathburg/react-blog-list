70e063a0bf5f5d410e4058637f36ae26ab7b75dd
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-19 16:41:37 -0700

    ci fixes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,7 @@
-import React, { useState } from 'react';
 import { usePosts } from '../../hooks/usePosts';
-import { client, checkError, getPosts } from '../../services/client';
 
 export default function Main() {
   const posts = usePosts();
-  console.log(posts);
   return (
     <div>
       {posts.map(({ id, title, subtitle, text, image }) => (
