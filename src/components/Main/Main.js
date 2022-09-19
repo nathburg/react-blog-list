ae906beb07d99c0555970ebd5f6dc147e9a7929a
Nathan Burgess <nathans.email.address@gmail.com>
2022-09-19 15:07:06 -0700

    learning secrets

---
+++ src/components/Main/Main.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { client, checkError } from '../../services/client';
+
+export default function Main() {
+  // const data = checkError(client.from('blogs').select('*'));
+  console.log(REACT_APP_);
+  return (
+    <div>Main</div>
+  );
+}
--- src/services/client.js
+++ src/services/client.js
@@ -1,4 +1,7 @@
+import React from 'react';
 import { createClient } from '@supabase/supabase-js';
+
+
 export const client = createClient(
   process.env.REACT_APP_SUPABASE_URL,
   process.env.REACT_APP_SUPABASE_KEY
